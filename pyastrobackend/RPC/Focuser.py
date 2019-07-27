54cd59aaf82267400cb7a69bd0057b46f3624484
Michael Fulbright <mike.fulbright@pobox.com>
2019-07-27 14:42:12 -0400

    Fixes for RPC support including focuser

---
+++ pyastrobackend/RPC/Focuser.py
@@ -0,0 +1,316 @@
+""" RPC Camera solution """
+
+import json
+import time
+import queue
+import select
+import socket
+import logging
+import weakref
+
+from ..BaseBackend import BaseFocuser
+
+from pyastrobackend.RPC.RPCDeviceBase import RPCDeviceThread, RPCDevice
+
+class RPCFocuserThread(RPCDeviceThread):
+    def __init__(self, port, user_data, *args, **kwargs):
+        super().__init__(port, user_data, *args, **kwargs)
+
+    def run(self):
+        logging.info('RPCFocuserThread Started!')
+
+        while True:
+            # clear out event queue
+            while True:
+                try:
+                    self.event_queue.get_nowait()
+                except queue.Empty:
+                    break
+
+            logging.info('Connecting to server')
+            while True:
+                try:
+                    self.rpc_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    logging.info('Attempting connect')
+                    self.rpc_socket.connect(('127.0.0.1', self.port))
+                    logging.info('Success!')
+                    break
+                except ConnectionRefusedError:
+                    logging.error('Failed to connect to RPC Server')
+                    self.rpc_socket.close()
+
+                time.sleep(5)
+
+            logging.debug('Sending connect event to queue')
+            cdict = { 'Event' : 'Connected' }
+            self.event_queue.put(cdict)
+
+            logging.info('Waiting on data')
+            quit = False
+            while not quit:
+                #logging.info('A')
+
+                # check if time for status update request
+                # if False and self.status_request_interval > 0:
+                    # if time.time() - self.last_status_request_timestamp > self.status_request_interval:
+                        # # request status
+                        # logging.debug('Sending getstatus request')
+                        # self.queue_rpc_command('getstatus', {})
+                        # self.last_status_request_timestamp = time.time()
+
+                read_list = [self.rpc_socket]
+                readable, writable, errored = select.select(read_list, [], [], 0.5)
+                #logging.info('B')
+
+                if len(readable) > 0:
+                    logging.info(f'reading data readable={readable}')
+
+                    with self._lock:
+                        try:
+                            j = self.read_json()
+                            logging.info(f'length of message = {len(j)}')
+                            logging.debug(f'j = {j}')
+                        except (ConnectionResetError, ConnectionAbortedError, BrokenPipeError):
+                            logging.error('RPCClient: server reset connection!')
+                            self.server_disconnected()
+                            quit = True
+                            j = ''
+
+                        if len(j) == 0:
+                            logging.warning('Received select for read 0 bytes!')
+                        else:
+                            jdict = json.loads(j)
+                            event = jdict.get('Event', None)
+                            req_id = jdict.get('id', None)
+                            #logging.info(f'{event} {req_id}')
+                            if req_id is not None:
+                                logging.debug(f'Received response {repr(jdict)[:60]}')
+                                logging.debug('appending response to list')
+                                self.responses.append(jdict)
+                                logging.debug(f'req_id = {req_id}')
+                                self.emit('Response', req_id)
+                            elif event is not None:
+                                logging.debug(f'Received event {event}')
+                                if event == 'Connection':
+                                    logging.debug('Recv Connection event')
+                                    self.emit(event)
+                            else:
+                                logging.warning(f'RPCClient: received JSON {jdict} with no event or id!')
+
+                # send any commands
+                try:
+                    rpccmd = self.command_queue.get(block=False)
+                except queue.Empty:
+                    rpccmd = None
+
+                if rpccmd is not None:
+                    logging.debug(f'Recvd command from queue {rpccmd}')
+                    cmd, edict = rpccmd
+                    cdict = { 'method' : cmd }
+                    jdict = {**cdict, **edict}
+                    jmsg = str.encode(json.dumps(jdict) + '\n')
+                    logging.debug(f'Sending json rpc = {jmsg}')
+                    try:
+                        self.rpc_socket.sendall(jmsg)
+                    except (ConnectionResetError, ConnectionAbortedError, BrokenPipeError):
+                        logging.error(f'RPCClient: connection reset sending poll response!')
+                        self.server_disconnected()
+                        quit = True
+                    except Exception as e:
+                        logging.error(f'RPCClient: exception sending poll response!')
+                        self.server_disconnected()
+                        quit = True
+
+
+            # fell out so close socket and try to reconnect
+            logging.debug('RPCClient: Fell out of main loop closing socket')
+            try:
+                self.rpc_socket.close()
+            except:
+                logging.error(f'RPCClient: Error closing rpc_socket={self.rpc_socket}', exc_info=True)
+
+
+class Focuser(RPCDevice, BaseFocuser):
+    def __init__(self, backend=None):
+        super().__init__(backend)
+
+        self.rpc_manager = RPCFocuserThread(8800, None)
+        self.rpc_manager.event_callbacks.append(self.event_callback)
+
+    def event_callback(self, event, *args):
+        logging.debug(f'Focsuer event_callback: {event} {args})')
+        if event == 'Connection':
+            self.connected = True
+        elif event == 'Response':
+            req_id = args[0]
+            logging.debug(f'event_callback: req_id = {req_id}')
+
+       
+    def wait_for_server(self, reqid, timeout=15):
+        # FIXME this shouldn't be a problem unless RPC Server dies
+        # FIXME add timeout
+        # block until we get answer
+        resp = None
+        waited = time.time()
+        while (time.time() - waited) < timeout:
+            #logging.debug('waiting...')
+            resp = self.rpc_manager.check_rpc_command_status(reqid)
+            if resp is not None:
+                break
+            time.sleep(0.1)    
+
+        if resp is None:
+            logging.error('RPC wait for server: resp is None!')
+
+        return resp         
+        
+
+    def get_absolute_position(self):
+
+        logging.info(f'RPC:get_absolute_position image')
+
+        rc = self.send_server_request('focuser_get_absolute_position', None)
+
+        if not rc:
+            logging.error('RPC:get_absolute_position - error')
+            return False
+
+        resp = self.wait_for_server(rc)
+
+        # FIXME parse out status?
+        status = 'result' in resp
+
+        logging.info(f'RPC get_absolute_position status/resp = {status} {resp}')
+
+        if not status:
+            logging.warning('RPC:get_absolute_position() - error getting settings!')
+            return False
+
+        result = resp['result']
+        abs_pos = result.get('absolute_position', None)
+        logging.debug(f'RPC:get_absolute_position() returns {abs_pos}')
+        return abs_pos
+
+    def get_max_absolute_position(self):
+        logging.info(f'RPC:get_max_absolute_position image')
+
+        rc = self.send_server_request('focuser_get_max_absolute_position', None)
+
+        if not rc:
+            logging.error('RPC:get_max_absolute_position - error')
+            return False
+
+        resp = self.wait_for_server(rc)
+
+        # FIXME parse out status?
+        status = 'result' in resp
+
+        logging.info(f'RPC get_max_absolute_position status/resp = {status} {resp}')
+
+        if not status:
+            logging.warning('RPC:get_max_absolute_position() - error getting settings!')
+            return False
+
+        result = resp['result']
+        max_abs_pos = result.get('max_absolute_position', None)
+        logging.debug(f'RPC:get_max_absolute_position() returns {max_abs_pos}')
+        return max_abs_pos
+
+    def get_current_temperature(self):
+        logging.info(f'RPC:get_current_temperature image')
+
+        rc = self.send_server_request('focuser_get_current_temperature', None)
+
+        if not rc:
+            logging.error('RPC:get_current_temperature - error')
+            return False
+
+        resp = self.wait_for_server(rc)
+
+        # FIXME parse out status?
+        status = 'result' in resp
+
+        logging.info(f'RPC get_current_temperature status/resp = {status} {resp}')
+
+        if not status:
+            logging.warning('RPC:get_current_temperature() - error getting settings!')
+            return False
+
+        result = resp['result']
+        cur_temp = result.get('current_temperature', None)
+        logging.debug(f'RPC:get_current_temperature() returns {cur_temp}')
+        return cur_temp
+
+    def is_moving(self):
+        logging.info(f'RPC:is_moving image')
+
+        rc = self.send_server_request('focuser_is_moving', None)
+
+        if not rc:
+            logging.error('RPC:is_moving - error')
+            return False
+
+        resp = self.wait_for_server(rc)
+
+        # FIXME parse out status?
+        status = 'result' in resp
+
+        logging.info(f'RPC is_moving status/resp = {status} {resp}')
+
+        if not status:
+            logging.warning('RPC:is_moving() - error getting settings!')
+            return False
+
+        result = resp['result']
+        is_moving = result.get('focuser_is_moving', None)
+        logging.debug(f'RPC:is_moving() returns {is_moving}')
+        return is_moving
+
+    def stop(self):
+        logging.info(f'RPC:focuser_stop')
+
+        rc = self.send_server_request('focuser_stop', None)
+
+        if not rc:
+            logging.error('RPC:stop - error')
+            return False
+
+        resp = self.wait_for_server(rc)
+
+        # FIXME parse out status?
+        status = 'result' in resp
+
+        logging.info(f'RPC focuser_stop status/resp = {status} {resp}')
+
+        if not status:
+            logging.warning('RPC:focuser_stop - error getting settings!')
+            return False
+
+        #FIXME need to look at result code
+        return True
+
+    def move_absolute_position(self, abspos):
+        logging.info(f'RPC:move_absolute_position to {abspos} ')
+
+        paramdict = {}
+        paramdict['params'] = {}
+        paramdict['params']['absolute_position'] = abspos
+        rc = self.send_server_request('focuser_move_absolute_position', paramdict)
+
+        if not rc:
+            logging.error('RPC:move_absolute_position - error')
+            return False
+
+        resp = self.wait_for_server(rc)
+
+        # FIXME parse out status?
+        status = 'result' in resp
+
+        logging.info(f'RPC move_absolute_position status/resp = {status} {resp}')
+
+        if not status:
+            logging.warning('RPC:move_absolute_position - error getting settings!')
+            return False
+
+        #FIXME need to look at result code
+        return True
---
+++ pyastrobackend/RPC/RPCDeviceBase.py
@@ -0,0 +1,317 @@
+""" RPC Device solution """
+
+import json
+import time
+import queue
+import select
+import socket
+import logging
+import weakref
+
+from threading import Thread, Lock
+
+
+class RPCDeviceThread(Thread):
+    def __init__(self, port, user_data, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.port = port
+        self.user_data = user_data
+
+        # this make the thread die when sys.exit() called
+        self.daemon = True
+        self.initialize()
+
+    def initialize(self):
+        self.daemon = True
+        self.socket = None
+        self.buffer = ''
+
+        self.rpc_request_id = 0
+
+        self._lock = Lock()
+        self.command_queue = queue.Queue()
+        self.event_queue = queue.Queue()
+        self.responses = []
+
+        # FIXME need weakrefs?
+        self.event_callbacks = []
+
+    def close(self):
+        if self.socket is not None:
+            self.socket.close()
+            self.initialize()
+
+    def emit(self, event, *args):
+        logging.debug(f'emit: {self.event_callbacks}')
+        for cb in self.event_callbacks:
+            logging.debug(f'emit: {cb} {event} {args}')
+            cb(event, *args)
+
+    def run(self):
+        logging.info('RPCCameraThread Started!')
+
+        while True:
+            # clear out event queue
+            while True:
+                try:
+                    self.event_queue.get_nowait()
+                except queue.Empty:
+                    break
+
+            logging.info('Connecting to server')
+            while True:
+                try:
+                    self.rpc_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    logging.info('Attempting connect')
+                    self.rpc_socket.connect(('127.0.0.1', self.port))
+                    logging.info('Success!')
+                    break
+                except ConnectionRefusedError:
+                    logging.error('Failed to connect to RPC Server')
+                    self.rpc_socket.close()
+
+                time.sleep(5)
+
+            logging.debug('Sending connect event to queue')
+            cdict = { 'Event' : 'Connected' }
+            self.event_queue.put(cdict)
+
+            logging.info('Waiting on data')
+            quit = False
+            while not quit:
+                #logging.info('A')
+
+                # check if time for status update request
+                # if False and self.status_request_interval > 0:
+                    # if time.time() - self.last_status_request_timestamp > self.status_request_interval:
+                        # # request status
+                        # logging.debug('Sending getstatus request')
+                        # self.queue_rpc_command('getstatus', {})
+                        # self.last_status_request_timestamp = time.time()
+
+                read_list = [self.rpc_socket]
+                readable, writable, errored = select.select(read_list, [], [], 0.5)
+                #logging.info('B')
+
+                if len(readable) > 0:
+                    logging.info(f'reading data readable={readable}')
+
+                    with self._lock:
+                        try:
+                            j = self.read_json()
+                            logging.info(f'length of message = {len(j)}')
+                            logging.debug(f'j = {j}')
+                        except (ConnectionResetError, ConnectionAbortedError, BrokenPipeError):
+                            logging.error('RPCClient: server reset connection!')
+                            self.server_disconnected()
+                            quit = True
+                            j = ''
+
+                        if len(j) == 0:
+                            logging.warning('Received select for read 0 bytes!')
+                        else:
+                            jdict = json.loads(j)
+                            event = jdict.get('Event', None)
+                            req_id = jdict.get('id', None)
+                            #logging.info(f'{event} {req_id}')
+                            if req_id is not None:
+                                logging.debug(f'Received response {repr(jdict)[:60]}')
+                                logging.debug('appending response to list')
+                                self.responses.append(jdict)
+                                logging.debug(f'req_id = {req_id}')
+                                self.emit('Response', req_id)
+                            elif event is not None:
+                                logging.debug(f'Received event {event}')
+                                if event == 'Connection':
+                                    logging.debug('Recv Connection event')
+                                    self.emit(event)
+                            else:
+                                logging.warning(f'RPCClient: received JSON {jdict} with no event or id!')
+
+                # send any commands
+                try:
+                    rpccmd = self.command_queue.get(block=False)
+                except queue.Empty:
+                    rpccmd = None
+
+                if rpccmd is not None:
+                    logging.debug(f'Recvd command from queue {rpccmd}')
+                    cmd, edict = rpccmd
+                    cdict = { 'method' : cmd }
+                    jdict = {**cdict, **edict}
+                    jmsg = str.encode(json.dumps(jdict) + '\n')
+                    logging.debug(f'Sending json rpc = {jmsg}')
+                    try:
+                        self.rpc_socket.sendall(jmsg)
+                    except (ConnectionResetError, ConnectionAbortedError, BrokenPipeError):
+                        logging.error(f'RPCClient: connection reset sending poll response!')
+                        self.server_disconnected()
+                        quit = True
+                    except Exception as e:
+                        logging.error(f'RPCClient: exception sending poll response!')
+                        self.server_disconnected()
+                        quit = True
+
+
+            # fell out so close socket and try to reconnect
+            logging.debug('RPCClient: Fell out of main loop closing socket')
+            try:
+                self.rpc_socket.close()
+            except:
+                logging.error(f'RPCClient: Error closing rpc_socket={self.rpc_socket}', exc_info=True)
+
+    def server_disconnected(self):
+        logging.error('RPCClient: server disconnection!')
+        self.rpc_socket.close()
+        self.buffer = ''
+        self.latest_status = ''
+        self.latest_status_timestamp = 0
+        cdict = { 'Event' : 'Disconnected' }
+        self.event_queue.put(cdict)
+
+    # given a received json for an event send a polling response to reset timer
+    def send_polling_response(self):
+        logging.info(f'sending_polling_response for event ')
+        #poll_cmd = {}
+        #poll_cmd['request'] = 'polling'
+        #self.rpc_socket.sendall(str.encode(json.dumps(poll_cmd)+'\n'))
+        self.queue_rpc_command('polling', {})
+
+    def read_json(self):
+        """ read \n terminated JSON blocks """
+
+        #self.rpc_socket.settimeout(0.5)
+        ret = ''
+        while True:
+            #try:
+            if True:
+                data = self.rpc_socket.recv(4096)
+#                logging.debug(f'read_json(): len(data) = {len(data)} data = |{data}|')
+
+                if len(data) < 1:
+                    logging.warning('socket ran dry')
+
+                    # if we've received NOTHING this pass means socket closed
+                    if ret == '':
+                        raise BrokenPipeError('recv returned 0 bytes - connection lost!')
+
+                    break
+
+                data = data.decode()
+                self.buffer += data
+                #print('self.buffer = |', self.buffer, '|')
+
+            # find first '{' and '\n' and see if in between contains a valid json block
+#            logging.debug(f'after read buffer = |{self.buffer}|')
+            try:
+                json_start = self.buffer.index('{')
+            except ValueError:
+                continue
+
+            try:
+                json_end = self.buffer[json_start:].index('\n')
+            except ValueError:
+                continue
+
+            #print('json start/end = ', json_start, json_start + json_end)
+
+            ret = self.buffer[json_start : json_start + json_end]
+            #print('json message      ->', ret, '<-')
+            self.buffer = self.buffer[ json_start + json_end : ]
+            #print('final read buffer ->', self.buffer, '<-')
+            break
+
+        return ret
+
+    def get_latest_status(self):
+        with self._lock:
+            ret = self.latest_status, self.latest_status_timestamp
+
+        #print('-----------------------\n', ret)
+        return ret
+
+    def queue_rpc_command(self, cmd, argsdict):
+        """
+        Accept rpc command and dictionary of arguments and creates the json
+        request dictionary and submits to command queue for rpc client thread.
+        """
+
+        #FIXME This works for our simple case but seems unclear if 'OK'
+        self.rpc_request_id += 1
+        jdict = { 'id' : self.rpc_request_id, **argsdict}
+        self.command_queue.put((cmd, jdict))
+
+        return self.rpc_request_id
+
+    def check_rpc_command_status(self, req_id):
+        """
+        See if response available for request id req_id and returns it.
+        Removes from list of requests.
+        """
+
+        for resp in self.responses:
+            if resp.get('id', None) == req_id:
+                logging.debug(f'Found response for request id {req_id}')
+                self.responses.remove(resp)
+                return resp
+
+        return None
+
+class RPCDevice:
+    def __init__(self, backend=None):
+        super().__init__()
+
+        self.connected = False
+        self.rpc_manager = None
+        self.port = 8800
+
+        # need to have code like this to setup the thread that talks to RPC server
+        # and point to event handler
+        #self.rpc_manager = RPCDeviceThread(8800, None)
+        #self.rpc_manager.event_callbacks.append(self.event_callback)
+
+    def has_chooser(self):
+        return False
+
+    def show_chooser(self, last_choice):
+        logging.warning('RPC Device Backend: no show_chooser()!')
+        return None
+
+    # name is currently ignored
+    def connect(self, name):
+        if self.rpc_manager is not None and self.rpc_manager.is_alive():
+            logging.error('RPCDevice.connect(): rpc_manager is not None! and running')
+            return False
+
+        logging.info(f'RPC Device connect: Connecting to RPCServer 127.0.0.1:{self.port}')
+
+        logging.info('Starting RPC Device manager thread')
+        self.rpc_manager.start()
+
+        while True:
+            logging.info('Waiting on connection')
+            if self.connected:
+                break
+            time.sleep(1)
+
+        return True
+
+    def disconnect(self):
+        self.rpc_manager.close()
+        self.connected = False
+
+    def is_connected(self):
+        return self.connected
+
+    def event_callback(self, event, *args):
+        logging.debug(f'event_callback: {event} {args})')
+        logging.warning('replace event_callback with custom')
+
+    def send_server_request(self, req, paramsdict=None):
+        logging.debug(f'send_server_req: {req} {paramsdict}')
+        if paramsdict is None:
+            paramsdict = {}
+
+        rc = self.rpc_manager.queue_rpc_command(req, paramsdict)
+        logging.debug(f'send_server_req: queue_rpc_command returned {rc}')
+        return rc
