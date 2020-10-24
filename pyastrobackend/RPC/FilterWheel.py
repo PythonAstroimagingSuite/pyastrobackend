a88c6da0f1da8254892680acb3a2476451e4c817
Michael Fulbright <mike.fulbright@pobox.com>
2020-10-24 16:26:45 -0400

    Add GPL v3 license to files and include LICENSE.txt

--- pyastrobackend/RPC/FilterWheel.py
+++ pyastrobackend/RPC/FilterWheel.py
@@ -4,8 +4,25 @@ from ..BaseBackend import BaseFilterWheel
 
 from pyastrobackend.RPC.RPCDeviceBase import RPCDeviceThread, RPCDevice
 
-# 0 = none, higher shows more
-LOG_SERVER_TRAFFIC = 1
+#
+# RPC filterwheel device
+#
+# Copyright 2020 Michael Fulbright
+#
+#
+#    pyastrobackend is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 class RPCFilterWheelThread(RPCDeviceThread):
     def __init__(self, port, user_data, *args, **kwargs):
